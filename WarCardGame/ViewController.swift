0fa8dac214fee0b0802435ec781c90c126a1a429
Sunny Patel <sunny33p@gmail.com>
2019-09-15 13:17:02 +1000

    Initial Commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Sunny Patel on 15/9/19.
+//  Copyright © 2019 Sunny Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
